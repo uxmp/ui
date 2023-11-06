5c2d17969ee8f7dcebc42020540ed7e37d943190
Daniel Jakob <github@usox.org>
2023-11-06 19:21:29 +0100

    Update some types in AuthStorage

--- src/components/Store/Modules/AuthStorage.ts
+++ src/components/Store/Modules/AuthStorage.ts
@@ -1,6 +1,14 @@
 import UserInterface from "../../../model/UserInterface";
 import AclEnum from "../../Lib/Enum/AclEnum";
 
+export interface AuthStorageState {
+  token: string,
+  user: UserInterface|null,
+  language: string,
+  temporaryPlaylistId: string|null,
+  acl: Array<Number>,
+}
+
 const getDefaultState = () => {
   return {
     token: '' as string,
@@ -15,51 +23,51 @@ export default {
   namespaced: true,
   state: getDefaultState(),
   getters: {
-    isLogged: state => state.token !== '',
-    getUser: state => state.user,
-    getToken: state => state.token,
-    getLanguage: state => state.language,
-    getTemporaryPlaylistId: state => state.temporaryPlaylistId,
-    getAcl: state => state.acl,
-    isAdmin: (state): boolean => {
+    isLogged: (state: AuthStorageState): boolean => state.token !== '',
+    getUser: (state: AuthStorageState): null|UserInterface => state.user,
+    getToken: (state: AuthStorageState): string => state.token,
+    getLanguage: (state: AuthStorageState): string => state.language,
+    getTemporaryPlaylistId: (state: AuthStorageState): null|string => state.temporaryPlaylistId,
+    getAcl: (state: AuthStorageState): Array<Number> => state.acl,
+    isAdmin: (state: AuthStorageState): boolean => {
       return state.acl.includes(AclEnum.USER_EDIT)
     }
   },
   mutations: {
-    SET_TOKEN: (state, token: string) => {
+    SET_TOKEN: (state: AuthStorageState, token: string): void => {
       state.token = token;
     },
-    SET_USER: (state, user: UserInterface) => {
+    SET_USER: (state: AuthStorageState, user: UserInterface): void => {
       state.user = user;
     },
-    RESET: state => {
+    RESET: (state: AuthStorageState): void => {
       Object.assign(state, getDefaultState());
     },
-    SET_LANGUAGE: (state, language: string) => {
+    SET_LANGUAGE: (state: AuthStorageState, language: string): void => {
       state.language = language
     },
-    SET_TEMPORARY_PLAYLIST_ID: (state, temporaryPlaylistId: null|string) => {
+    SET_TEMPORARY_PLAYLIST_ID: (state: AuthStorageState, temporaryPlaylistId: null|string): void => {
       state.temporaryPlaylistId = temporaryPlaylistId
     },
-    SET_ACL: (state, acl: Array<number>) => {
+    SET_ACL: (state: AuthStorageState, acl: Array<number>): void => {
       state.acl = acl
     }
   },
   actions: {
-    login: ({ commit }, { token, user }) => {
+    login: ({ commit }, { token, user }): void => {
       commit('SET_TOKEN', token);
       commit('SET_USER', user);
     },
-    logout: ({ commit }) => {
+    logout: ({ commit }): void => {
       commit('RESET', '');
     },
-    setLanguage: ({ commit }, { language }) => {
+    setLanguage: ({ commit }, { language } ): void => {
       commit('SET_LANGUAGE', language)
     },
-    setTemporaryPlaylistId: ({ commit }, {temporaryPlaylistId}) => {
+    setTemporaryPlaylistId: ({ commit }, {temporaryPlaylistId}): void => {
       commit('SET_TEMPORARY_PLAYLIST_ID', temporaryPlaylistId)
     },
-    setAcl: ({commit}, {acl}) => {
+    setAcl: ({commit}, {acl}): void => {
       commit('SET_ACL', acl)
     },
   }
